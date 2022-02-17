a7a2a603e8077a45dcb1f90964880eff4c3e3d33
besarkutleshi <besar.kutleshi@keds-energy.com>
2022-02-17 16:22:14 +0100

    Project finished

--- PlatformService/SyncDataServices/Grpc/GrpcPlatformService.cs
+++ PlatformService/SyncDataServices/Grpc/GrpcPlatformService.cs
@@ -1,10 +1,7 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Grpc.Core;
 using PlatformService.Interfaces;
-using PlatformService.Models;
 
 namespace PlatformService.SyncDataServices.Grpc
 {
